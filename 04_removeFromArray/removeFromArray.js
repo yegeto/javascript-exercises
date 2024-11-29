3e1df44f18f50d76ec7389c9b0177989ae829d20
yegeto <115154804+yegeto@users.noreply.github.com>
2024-11-29 22:16:53 +0300

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...unwanted) {
+  const unwantedIndexArray = [];
+  for (const piece of unwanted) {
+    array.forEach((element, index) => {
+      if (piece === element) {
+        unwantedIndexArray.push(index);
+      }
+    });
+  }
+  unwantedIndexArray.sort((a, b) => b - a);
+  for (const element of unwantedIndexArray) {
+    array.splice(element, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
