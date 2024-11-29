863d59a4cd2809b7f769d360382a2610644b1fbe
yegeto <115154804+yegeto@users.noreply.github.com>
2024-11-29 14:55:38 +0300

    solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (input, repeatAmount) {
+  if (repeatAmount < 0) return "ERROR";
+  let output = "";
+  for (let i = 0; i < repeatAmount; i++) {
+    output += input;
+  }
+  return output;
 };
 
 // Do not edit below this line
