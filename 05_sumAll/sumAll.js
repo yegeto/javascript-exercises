6b2365d96559a4bf6db44b4874d868ad57e9e526
yegeto <115154804+yegeto@users.noreply.github.com>
2024-11-30 11:57:37 +0300

    solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (...numberInputs) {
+  const checkInputsAreIntegers = (array) => {
+    return array.every((number) => Number.isInteger(number) && number >= 0);
+  };
+  if (checkInputsAreIntegers(numberInputs)) {
+    const sortedIntegers = numberInputs.sort((a, b) => a - b);
+    let result = 0;
+    for (let i = sortedIntegers[0]; i <= sortedIntegers[1]; i++) {
+      result += i;
+    }
+    return result;
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
