b83ebead3ea3cd08d85f54428f871d4e2fca5e8e
yegeto <115154804+yegeto@users.noreply.github.com>
2024-11-30 12:37:37 +0300

    solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (!(year % 400) || (!(year % 4) && year % 100)) return true;
+  return false;
 };
 
 // Do not edit below this line
