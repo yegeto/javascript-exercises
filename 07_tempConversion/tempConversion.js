b34335a64b64e142bb2ea23e66f72e341e41e9a0
yegeto <115154804+yegeto@users.noreply.github.com>
2024-11-30 13:07:44 +0300

    solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (inputYear) {
+  return Number((((inputYear - 32) * 5) / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (inputYear) {
+  return Number(((inputYear * 9) / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
