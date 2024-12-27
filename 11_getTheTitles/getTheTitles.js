49702b04a4274aa019d74f5dd083c3b0710a4d36
yegeto <115154804+yegeto@users.noreply.github.com>
2024-12-27 14:05:43 +0300

    solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const bookTitleArr = [];
+  for (const book of books) {
+    bookTitleArr.push(book.title);
+  }
+  return bookTitleArr;
 };
 
 // Do not edit below this line
