d988604d19fa5f3e93aa7054988f5391a2c923b4
yegeto <115154804+yegeto@users.noreply.github.com>
2024-12-27 13:34:26 +0300

    solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+  let line;
+  if (typeof input !== "number") {
+    line = parseInt(input);
+  } else {
+    line = input;
+  }
+  if (line < 0) return "OOPS";
+  if (line == 0) return 0;
+  if (line === 1) return 1;
+  if (line === 2) return 1;
 
+  return fibonacci(line - 1) + fibonacci(line - 2);
 };
 
 // Do not edit below this line
