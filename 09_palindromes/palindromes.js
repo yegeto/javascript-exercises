c48a3b85b5e3582aaf174ce278d0efa2c07f4cb7
yegeto <115154804+yegeto@users.noreply.github.com>
2024-12-27 13:00:02 +0300

    solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  const removedUnwantedSpecs = input.replace(/[^\w]|_/g, "").toLowerCase();
+  const reversedInput = removedUnwantedSpecs.split("").reverse().join("");
+  return reversedInput === removedUnwantedSpecs;
 };
 
 // Do not edit below this line
