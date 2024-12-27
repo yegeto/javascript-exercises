7fe312dadf543af022e60d4c93c9abc14e549d36
yegeto <115154804+yegeto@users.noreply.github.com>
2024-12-27 17:15:30 +0300

    solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findAge = (person) => {
+  if (!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
+  return person.yearOfDeath - person.yearOfBirth;
+};
+const findTheOldest = function (peopleArr) {
+  return peopleArr.reduce((oldestPerson, currentPerson) => {
+    const personAge = findAge(currentPerson);
+    const oldestAge = findAge(oldestPerson);
+    return personAge > oldestAge ? currentPerson : oldestPerson;
+  });
 };
 
 // Do not edit below this line
